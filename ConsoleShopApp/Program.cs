1dc77c9b941fd7fa351f391c35c707f14346eabb
Justas383 <jgudeika@gmail.com>
2021-05-27 12:57:53 +0300

    Add project files.

---
+++ ConsoleShopApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleShopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
